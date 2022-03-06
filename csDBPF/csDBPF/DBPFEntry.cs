e73f6a00e72322544cedc3c75c4948ffca6a350a
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2022-03-06 17:08:38 -0500

    read and set entry data

--- csDBPF/csDBPF/DBPFEntry.cs
+++ csDBPF/csDBPF/DBPFEntry.cs
@@ -26,36 +26,59 @@ namespace csDBPF {
 			//set { _offset = value; }
 		}
 
-		private uint _uncompressedSize;
-		public uint uncompressedSize {
-			get { return _uncompressedSize; }
-			set { _uncompressedSize = value; }
-		}
-
-		private uint _compressedSize;
-		public uint compressedSize {
-			get { return _compressedSize; }
-			set { _compressedSize = value; }
-		}
-
 		private uint _index;
 		public uint indexPos {
 			get { return _index; }
 			//set { _index = value; }
 		}
 
+
+		/// <summary>
+		/// Uncompressed size of the entry data, in bytes.
+		/// </summary>
+		/// <remarks>
+		/// Initially all data is assumed to be compressed until the first bytes of data can be read to determine actual compression status.
+		/// </remarks>
+		private uint _uncompressedSize;
+		public uint uncompressedSize {
+			get { return _uncompressedSize; }
+			set { _uncompressedSize = value; }
+		}
+
+		/// <summary>
+		/// Compressed size of the entry data, in bytes.
+		/// </summary>
+		/// <remarks>
+		/// Initially all data is assumed to be compressed until the first bytes of data can be read to determine actual compression status.
+		/// </remarks>
+		private uint _compressedSize;
+		public uint compressedSize {
+			get { return _compressedSize; }
+			set { _compressedSize = value; }
+		}
+
+
 		private bool _isCompressed;
 		public bool isCompressed {
 			get { return _isCompressed; }
-			//set { isCompressed = value; }
+			set { isCompressed = value; }
+		}
+
+		private byte[] _data;
+		public byte[] data {
+			get { return _data; }
+			set { _data = value; }
 		}
 
 
 
 
 
-
 		// Constructor
+		/// <summary>
+		/// Create a new DBPFEntry object.
+		/// </summary>
+		/// <param name="tgi"><see cref="DBPFTGI"/> object representing the entry</param>
 		public DBPFEntry(DBPFTGI tgi) {
 			_tgi = tgi;
 		}
@@ -63,9 +86,9 @@ namespace csDBPF {
 		/// <summary>
 		/// Create a new DBPFEntry object.
 		/// </summary>
-		/// <param name="tgi">TGI object representing the entry.</param>
+		/// <param name="tgi"><see cref="DBPFTGI"/> object representing the entry</param>
 		/// <param name="offset">Offset (location) of the entry within the DBPF file</param>
-		/// <param name="size">Size of data for the entry, in bits.</param> //TODO - what are the units of entry size?
+		/// <param name="size">Size of data for the entry, in bytes. Set both compressed and uncompressed size to this until the data is set to make a determination</param>
 		/// <param name="index">Entry position in the file. 0-n</param>
 		public DBPFEntry(DBPFTGI tgi, uint offset, uint size, uint index) {
 			if (tgi == null) {
@@ -76,18 +99,15 @@ namespace csDBPF {
 			}
 			_offset = offset;
 			_uncompressedSize = size;
+			_isCompressed = true;
+			_compressedSize = size;
 			_index = index;
-
-			//We know the offset and size of data, so examine it to determine compression characteristics
-			byte[] data = new byte[size];
-
-
-			_isCompressed = false; //TODO implement isCompressed fields for DBPFEntry
+			//Note: for other properties, we cannot know them until after the data is read and set
 		}
 
 		public override string ToString() {
 			StringBuilder sb = new StringBuilder(_tgi.ToString());
-			sb.AppendLine($" IndexPosition: {_index}, Offset: {_offset}, Size: {_compressedSize}, ");
+			sb.AppendLine($", IndexPos: {_index}, Offset: {_offset}, uSize: {_uncompressedSize}, Compr: {_isCompressed}, cSize: {_compressedSize} ");
 			return sb.ToString();
 		}
 
