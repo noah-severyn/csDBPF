4948491417ad1dfb6b1fb4692e243f7c08fc6b5c
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2022-03-12 15:15:32 -0500

    added reverse byte function for ushorts

--- csDBPF/csDBPF/DBPFUtil.cs
+++ csDBPF/csDBPF/DBPFUtil.cs
@@ -36,15 +36,27 @@ namespace csDBPF {
 
 
 		/// <summary>
-		/// Reverses the byte order for a uint. Example: 1697917002 (0x 65 34 28 4A) returns 1244148837 (0x 4A 28 34 65)
+		/// Reverses the byte order for a uint. Example: 1697917002 (0x6534284A) returns 1244148837 (0x4A283465)
 		/// </summary>
 		/// <remarks>
 		/// See:https://stackoverflow.com/a/18145923/10802255
 		/// </remarks>
-		/// <param name="value">Integer value to reverse</param>
-		/// <returns></returns>
+		/// <param name="value">Value to reverse</param>
+		/// <returns>Reversed uint</returns>
 		public static uint ReverseBytes(uint value) {
 			return (value & 0x000000FFU) << 24 | (value & 0x0000FF00U) << 8 | (value & 0x00FF0000U) >> 8 | (value & 0xFF000000U) >> 24;
+		}
+
+		/// <summary>
+		/// Reverses the byte order for a ushort. Example: 3 (0x0003) returns 768 (0x0300)
+		/// </summary>
+		/// <remarks>
+		/// See:https://stackoverflow.com/a/18145923/10802255
+		/// </remarks>
+		/// <param name="value">Value to reverse</param>
+		/// <returns>Reversed ushort</returns>
+		public static ushort ReverseBytes(ushort value) {
+			return (ushort) ((value & 0x00FFU) << 8 | (value & 0xFF00U) >> 8);
 		}
 
 		/// <summary>
