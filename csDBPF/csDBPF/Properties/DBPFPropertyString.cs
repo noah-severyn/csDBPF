f97789035d6e82da94b62ec45d65984dd8fac5ce
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2022-03-13 17:25:14 -0400

    Create DBPFPropertyString.cs

---
+++ csDBPF/csDBPF/Properties/DBPFPropertyString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csDBPF.Properties {
+	public class DBPFPropertyString : DBPFProperty {
+
+
+		public DBPFPropertyString(DBPFPropertyDataType dataType) : base(dataType) {
+
+			//public DirectDBPFEntry(DBPFTGI tgi, uint offset, uint size, uint index) : base(tgi) {
+		}
+
+	}
+}
