4b13f306c650d42bab86b6c4056d7bc0988dbf19
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2022-03-17 22:22:13 -0400

    cleanup setting decoded values

--- csDBPF/csDBPF/Properties/DBPFPropertyString.cs
+++ csDBPF/csDBPF/Properties/DBPFPropertyString.cs
@@ -17,12 +17,11 @@ namespace csDBPF.Properties {
 
 
 		/// <summary>
-		/// Number of reps of the data type in this property. This is always 1.
+		/// Number of repetitions of the data type in this property. Describes the number of chars in the string (length of string).
 		/// </summary>
-		private int _numberOfReps;
-		public override int numberOfReps {
+		private uint _numberOfReps;
+		public override uint numberOfReps {
 			get { return _numberOfReps; }
-			set { _numberOfReps = value; }
 		}
 
 
@@ -42,12 +41,13 @@ namespace csDBPF.Properties {
 
 
 		/// <summary>
-		/// The byte array of base data for the property. When this is set, <see cref="DBPFPropertyString.valuesDecoded"/> is also set to the equivalent value.
+		/// The byte array of base data for the property. When this is set, <see cref="valuesDecoded"/> is also set to the equivalent value.
 		/// </summary>
 		private byte[] _values;
 		public override byte[] values {
 			get { return _values; }
 			set {
+				_numberOfReps = (uint) value.Length;
 				_values = value;
 				_valuesDecoded = DBPFUtil.StringFromByteArray(value);
 			}
@@ -55,7 +55,7 @@ namespace csDBPF.Properties {
 
 
 		/// <summary>
-		/// When decoded, <see cref="DBPFPropertyString.values"/> returns a string. When this is set, <see cref="DBPFPropertyString.values"/> is also set to the equivalent value.
+		/// When decoded, <see cref="DBPFPropertyString.values"/> returns a string. When this is set, <see cref="values"/> is also set to the equivalent value.
 		/// </summary>
 		private string _valuesDecoded;
 		public override object valuesDecoded {
@@ -64,31 +64,13 @@ namespace csDBPF.Properties {
 				Type t = value.GetType();
 
 				//If type(value) is string then directly set the decoded value
-				if (t == "".GetType()) {
+				if (t != "".GetType()) {
+					throw new ArgumentException($"Property {this} cannot apply set the value field to type of {t}.");
+				} else {
+					_numberOfReps = (uint) ((string) value).Length;
 					_values = DBPFUtil.StringToByteArray((string) value);
 					_valuesDecoded = (string) value;
-					return;
 				}
-
-				//Otherwise check if value is an array of bytes
-				if (!t.IsArray || t.GetElementType() != byte.MinValue.GetType()) {
-					throw new ArgumentException($"An array of {t.GetElementType().Name} was provided when {Type.GetType("System.String")} is expected");
-				} else {
-					ArrayList result = new ArrayList(); //Use array list because we do not quite know the length
-					IEnumerable e = value as IEnumerable;
-					int idx = 0;
-					if (e != null) {
-						foreach (object item in e) {
-							result.Add((byte) item);
-							idx++;
-						}
-
-						_values = result.ToArray(typeof(byte)) as byte[];
-						_valuesDecoded = string.Join("", result);
-						return;
-					}
-				}
-				throw new ArgumentException($"Property {this} cannot apply set the value field to type of {t}.");
 			}
 		}
 
