d345ca3a8be9524ea18cb463170381af69a8d37f
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2022-06-06 22:41:27 -0400

    scaffolding and enums

--- csDBPF/csDBPF/DBPFEntryDecoding.cs
+++ csDBPF/csDBPF/DBPFEntryDecoding.cs
@@ -11,7 +11,7 @@ namespace csDBPF {
 		/// <summary>
 		/// Decodes the compressed data into a dictionary of one or more <see cref="DBPFProperty"/>.
 		/// </summary>
-		/// <param name="cData">Compressed data</param>
+		/// <param name="cData">Compressed byte data</param>
 		/// <returns>Dictionary of <see cref="DBPFProperty"/> indexed by their order in the entry</returns>
 		internal static Dictionary<int, DBPFProperty> DecodeEntry_EXMP(byte[] cData) {
 			byte[] dData;
@@ -100,5 +100,101 @@ namespace csDBPF {
 
 
 
+		//https://wiki.sc4devotion.com/index.php?title=FSH_Format
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="cData">Compressed byte data</param>
+		/// <returns></returns>
+		internal static byte[] DecodeEntry_FSH(byte[] cData) {
+			byte[] dData;
+			if (DBPFCompression.IsCompressed(cData)) {
+				dData = DBPFCompression.Decompress(cData);
+			} else {
+				dData = cData;
+			}
+
+			//analyze fsh file header
+			uint identifer = BitConverter.ToUInt32(dData, 0);
+			int fileSize = BitConverter.ToInt32(dData, 4);
+			int entryCount = BitConverter.ToInt32(dData, 8);
+			int directoryID = BitConverter.ToInt32(dData, 12);
+
+			//analyze fsh directory
+			int offset = 16;
+			Dictionary<uint, int> FSHDirectory = new Dictionary<uint, int>();
+
+			for (int entry = 0; entry < entryCount; entry++) {
+				uint entryName = ByteArrayHelper.ReadBytesIntoUint(dData, offset);
+				offset += 4;
+				int entryOffset = BitConverter.ToInt32(dData, offset);
+				offset += 4;
+				FSHDirectory.Add(entryName, entryOffset);
+			}
+
+			//after the directory is built, we can look at each specific FSH entry in the file (there can be more than one)
+			//parse the header
+
+			//after header is bitmap / palette pixel / color information
+			//pallets generally are 256 length arrays of 1 byte
+			//bitmaps store pixel data in many ways
+			//fsh images can store raw data or microsoft dxtc compressed
+
+
+			//TODO - this is getting pretty complicated - it might be better to split this out into its own class along with the respective enums and FSH Header subclass. Create a new folder for filetypes (or something like that)
+		}
+
+
+		private class FSHHeader {
+			byte RecordID;
+			int BlockSize; //3 bytes only
+			ushort Width;
+			ushort Height;
+			//ushort XCenterLoc;
+			//ushort YCenterLoc;
+			//ushort XOffsetLoc;
+			//ushort YOffsetLoc;
+			byte[] data;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <see cref="https://wiki.sc4devotion.com/index.php?title=FSH_Format#Directory_ID_Values"/>
+		public enum FSH_DirectoryID {
+			Building = 0x47334444, //G354 - Building Textures
+			EverythingElse = 0x47323634, //G264 - Network Textures, Sim Textures, Sim heads, Sim animations, Trees, props, Base textures, Misc colors
+			Animation = 0x47323636, //G266 - 3d Animation textures (e.g.the green rotating diamond in loteditor.dat)
+			DispatchMarker = 0x47323930, //G290 - Dispatch marker textures
+			NetworkTransportModel = 0x71333135, //G315 - Small Sim texture, Network Transport Model Textures(trains, etc.)
+			UIEditor = 0x47494D58, //GIMX - UI Editor textures
+			BATgen = 0x47333434 //G344 - BAT gen texture maps
+		}
+
+		public enum FSH_BitmapCode {
+			EightBit_Indexed = 0x7B, //Type: 8-bit indexed, Palette: directly follows bitmap or uses global palette, Compression: none
+			ThirtyTwoBit_a8r8g8b8 = 0x7D, //Type: 32-bit A8R8G8B8, Palette: none, Compression: none
+			TwentyFourBit_a0r8g8b8 = 0x7F, //Type: 24-bit A0R8G8B8, Palette: none, Compression: none
+			SixteenBit_a1r5g5b5 = 0x7E, //Type:	16-bit A1R5G5B5, Palette: none, Compression: none
+			SixteenBit_a0r5g5b5 = 0x78, //Type:	16-bit A0R5G6B5, Palette: none, Compression: none
+			SixteenBit_a4r4g4b4 = 0x6D, //Type: 16-bit A4R4G4B4, Palette: none, Compression: none
+			DXT3_4x4_4bitAlpha = 0x61, //Type: DXT3 4x4 packed, 4-bit alpha, Palette: none, Compression: 4x4 grid compressed, half-byte per pixel
+			DXT3_4x4_1bitAlpha = 0x60, //Type: DXT3 4x4 packed, 1-bit alpha, Palette: none, Compression: 4x4 grid compressed, half-byte per pixel
+		}
+
+		public enum FSH_PaletteCode {
+			TwentyFourBitDOS = 0x22, //24-bit DOS
+			TwentyFourBit = 0x24, //24-bit 
+			SixteenBitNFSS = 0x29, //16-bit NFS5 
+			ThirtyTwoBit = 0x2A, //32-bit 
+			SixteenBit = 0x2D //16-bit
+		}
+
+		public enum FSH_TextCode {
+			StandardText = 0x6F, //Standard Text file
+			ArbitraryLength = 0x69, //ETXT of arbitrary length with full entry header 
+			LessThan16Bytes = 0x70, //ETXT of 16 bytes or less including the header 
+			PixelRegionHotspot = 0x7C //defined Pixel region Hotspot data for image.
+		}
 	}
 }
