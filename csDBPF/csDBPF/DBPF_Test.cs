99766e3b1be11cdb2390f55985168ebf8affd8f3
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2022-12-06 00:10:07 -0500

    added console entry point

---
+++ csDBPF/csDBPF/DBPF_Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csDBPF {
+	internal class DBPF_Test {
+
+		static void Main(string[] args) {
+			string path = "C:\\Users\\Administrator\\Documents\\SimCity 4\\Plugins\\new.dat";
+			string parksaura = "C:\\Users\\Administrator\\Documents\\SimCity 4\\Plugins\\z_DataView - Parks Aura.dat";
+			DBPFFile newdat = new DBPFFile(parksaura);
+			newdat.Save(path);
+		}
+	}
+}
