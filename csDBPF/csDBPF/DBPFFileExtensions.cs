f6f094beebf2a8f715a18e3571f11095da6a0c5c
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2024-04-04 20:19:44 -0400

    Create DBPFFileExtensions.cs

---
+++ csDBPF/csDBPF/DBPFFileExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using csDBPF;
+
+namespace csDBPF {
+    /// <summary>
+    /// A set of extension methods for working with DBPF items
+    /// </summary>
+    public static class DBPFFileExtensions {
+
+        /// <summary>
+        /// Decode all entries in the given list
+        /// </summary>
+        /// <param name="entries">List of entries to decode</param>
+        public static void DecodeEntries(this IEnumerable<DBPFEntry> entries) {
+            foreach (DBPFEntry entry in entries) {
+                entry.Decode();
+            }
+        }
+
+    }
+}
